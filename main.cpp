56e40b18dc979c05284fa9685bc602b6d3c3e5ee
d4nielxo <dhaddad3@students.avc.edu>
2026-03-22 09:48:57 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
-min = num1;
-if (num2 < min) {
-    min = num2;
-}
-if (num3 < min) {
-    min = num3;
-}
+#include <iostream>
+using namespace std;
 
-max = num1;
-if (num2 > max) {
-    max = num2;
-}
-if (num3 > max) {
-    max = num3;
-}
+int main() {
+    int num1, num2, num3;
+    int min, max, med;
 
-if (num1 != min && num1 != max) {
-    med = num1;
-} else if (num2 != min && num2 != max) {
-    med = num2;
-} else {
-    med = num3;
+    cout << "Enter three integer values: ";
+    cin >> num1 >> num2 >> num3;
+
+    min = num1;
+    if (num2 < min) {
+        min = num2;
+    }
+    if (num3 < min) {
+        min = num3;
+    }
+
+    max = num1;
+    if (num2 > max) {
+        max = num2;
+    }
+    if (num3 > max) {
+        max = num3;
+    }
+
+    if (num1 != min && num1 != max) {
+        med = num1;
+    } else if (num2 != min && num2 != max) {
+        med = num2;
+    } else {
+        med = num3;
+    }
+
+    cout << min << " " << med << " " << max << endl;
+
+    return 0;
 }
