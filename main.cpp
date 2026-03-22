491adedb218d77f92b9fcd9470c53698bb6c16ae
d4nielxo <dhaddad3@students.avc.edu>
2026-03-22 09:48:53 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
-//      **************************************************
-//      Find the min, max among three values
-//      Display three values in ascending order  min, other, max
-//     **************************************************
-#include <iostream>
-using namespace std;
-int main()
-{
-    int num1, num2, num3;
-    int min, max, med;
-    cout << "Enter three integer values: ";
-    cin >> num1 >> num2 >> num3;
+min = num1;
+if (num2 < min) {
+    min = num2;
+}
+if (num3 < min) {
+    min = num3;
+}
 
-    // TODO: Find min, max, and med among num1, num2, num3
+max = num1;
+if (num2 > max) {
+    max = num2;
+}
+if (num3 > max) {
+    max = num3;
+}
 
-    cout << min << " " << med << " " << max << endl;
+if (num1 != min && num1 != max) {
+    med = num1;
+} else if (num2 != min && num2 != max) {
+    med = num2;
+} else {
+    med = num3;
 }
